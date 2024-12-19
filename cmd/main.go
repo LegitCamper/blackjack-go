4c2d7c5a3015102e9165020b4e7917de0590daca
sawyer bristol <sawyerbristol@gmail.com>
2024-12-19 00:33:49 -0700

    can update hand

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,21 @@ import (
 )
 
 func main() {
-	http.Handle("/", templ.Handler(home()))
-	http.Handle("/basic_play", templ.Handler(basic_play()))
+	http.Handle("GET /", templ.Handler(home()))
 
 	assets := http.FileServer(http.Dir("assets/"))
-	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
+	http.Handle("GET /assets/", http.StripPrefix("/assets/", assets))
+
+	hand := new_hand()
+	shoe := new_shoe(6)
+	http.HandleFunc("GET /basic_play", func(w http.ResponseWriter, r *http.Request) {
+		hand.deal(&shoe)
+		basic_play(hand, shoe).Render(r.Context(), w)
+	})
+	http.HandleFunc("POST /basic_play/hit", func(w http.ResponseWriter, r *http.Request) {
+		hand.hit(&shoe)
+		show_hand(hand).Render(r.Context(), w)
+	})
 
 	fmt.Println("Listening on :3000")
 	err := http.ListenAndServe(":3000", nil)
